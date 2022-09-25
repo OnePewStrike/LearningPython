b4269537153eb5883a50c5ff15c84ee575c830c0
Godwin Duliente <rualgodwin@gmail.com>
2022-09-25 10:57:52 +0800

    practice parsing

--- DataMining/parse.csv.py
+++ DataMining/parse.csv.py
@@ -1,13 +1,27 @@
 import csv
 
+# with open('new_data_format.csv', 'r') as csv_file:
+#   csv_reader = csv.reader(csv_file, delimiter="\t")
+  
+#   for line in csv_reader:
+#     print(line)
+  
 with open('data.csv', 'r') as csv_file:
-  csv_reader = csv.reader(csv_file)
+  csv_reader = csv.DictReader(csv_file)
   
-  # next(csv_reader) // moves to next line
-  with open('new_names.csv', 'w') as new_file:
-    csv_writer = csv.writer(new_file, delimiter='-')
+  for line in csv_reader:
+    # print(line)
+    print(line['X1'])
   
-    for line in csv_reader:
-      csv_writer.writerow(line)
-      #print(line[2])
-      # print(line[2]) // 3rd column will be printed
+  
+# with open('data.csv', 'r') as csv_file:
+#   csv_reader = csv.reader(csv_file)
+  
+#   # next(csv_reader) // moves to next line
+#   with open('new_data_format.csv', 'w') as new_file:
+#     csv_writer = csv.writer(new_file, delimiter='\t')
+  
+#     for line in csv_reader:
+#       csv_writer.writerow(line)
+#       #print(line[2])
+#       # print(line[2]) // 3rd column will be printed
