346fdeb0165d6b40eac3286a16c253ca0a986f49
Godwin Duliente <rualgodwin@gmail.com>
2022-09-25 18:40:03 +0800

    practice mining

--- DataMining/dataMining.py
+++ DataMining/dataMining.py
@@ -1,6 +1,50 @@
 import csv
 
-with open("data.csv", "r") as infile:
-    reader = csv.reader(infile, delimiter=",")
-    header = next(reader)
-    print(header)
+
+def viewOriginalData():
+    with open('data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+            print(line)
+
+
+def solveByRemoveRows():
+    L = []
+    with open('data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            if row[1] == '' or row[2] == '' or row[3] == '' or row[4] == '':
+                continue
+            else:
+                L.append(row)
+
+        with open("new_data_format.csv", 'w') as new_file:
+            csv_writer = csv.writer(new_file, delimiter=",")
+            csv_writer.writerows(L)
+
+
+def solveByAverage():
+    L = []
+    total = None
+    with open('data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            header = next(csv_reader)
+            total = int(row[0])
+        print("\n\tThe total is {}", total)
+
+        for row in csv_reader:
+            if row[0] == '':
+                continue
+            else:
+                L.append(row)
+
+        with open("new_data_format.csv", 'w') as new_file:
+            csv_writer = csv.writer(new_file, delimiter=",")
+            csv_writer.writerows(L)
+
+
+solveByRemoveRows()
